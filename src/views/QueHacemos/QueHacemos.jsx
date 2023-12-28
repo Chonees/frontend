bc9ed509fd76a71d12c9f0321fbc17889a060877
MaximoVeltri <maximoveltri@gmail.com>
2023-12-28 13:47:44 -0300

    primer avance

---
+++ src/views/QueHacemos/QueHacemos.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+
+import "./QueHacemos.css";
+const QueHacemos= ()=>{
+    // Estado para controlar la visibilidad de las respuestas
+    const [mostrarRespuestas, setMostrarRespuestas] = useState([false, false, false]);
+
+      // Función para cambiar la visibilidad de una respuesta específica
+    const toggleRespuesta = (index) => {
+      setMostrarRespuestas((prevState) => {
+      const nuevosEstados = [...prevState];
+      nuevosEstados[index] = !nuevosEstados[index];
+      return nuevosEstados;
+    });
+  };
+    return (
+        <>        
+          <div>
+            <h1>
+              <span>
+                Lifestyle &amp; 
+              </span>
+
+              <span>
+                Functional 
+              </span>
+
+              <span>
+                Medicine. 
+              </span>
+            </h1>
+            <div>
+              <h2 >
+                Un abordaje de vanguardia para optimizar tu relación con tu salud y tu manera de vivir.
+              </h2>
+            </div>
+          </div>
+         <div>
+            <hr/>
+            <h2>
+              ¿Qué es el Lifestyle <br />&amp; Functional Medicine?
+            </h2>
+            <article>
+              <figure>
+                <img/>
+              </figure>
+
+              <p>
+                Una nueva solución para tu salud que va a la raíz de tus síntomas trabajando para prevenir, restaurar y revertir disfunciones crónicas relacionadas a tu estilo de vida.
+              </p>
+
+              <figure>
+                <img />
+              </figure>
+
+              <p>
+                Un abordaje científico que aplica los últimos avances en biología molecular, genómica nutricional, microbiología, inmunología nutricional y neuroendocrinología.
+              </p>
+
+              <figure>
+                <img/>
+              </figure>
+              <p>
+                Un campo que cuenta con un amplio respaldo internacional, liderado por Cleveland Clinic y la Clínica Mayo en Estados Unidos y organizaciones como BANT y IFM.
+              </p>
+            </article>
+          </div>
+          <div>
+            <h2>Nuestro abordaje</h2>
+            <div>
+              <h3>Pregunta 1</h3>
+              <button onClick={() => toggleRespuesta(0)}>Mostrar respuesta</button>
+              {mostrarRespuestas[0] && <p>Respuesta 1</p>}
+            </div>
+
+            <div>
+              <h3>Pregunta 2</h3>
+              <button onClick={() => toggleRespuesta(1)}>Mostrar respuesta</button>
+              {mostrarRespuestas[1] && <p>Respuesta 2</p>}
+            </div>
+
+            <div>
+              <h3>Pregunta 3</h3>
+              <button onClick={() => toggleRespuesta(2)}>Mostrar respuesta</button>
+              {mostrarRespuestas[2] && <p>Respuesta 3</p>}
+            </div>
+            <div>
+              <h3>Pilares de intervencion</h3>
+              <p>Trabajaremos en revitalizar, detoxificar, optimizar y recalibrar tu organismo de forma sistémica yendo a las causas detrás de tus síntomas mediante intervenciones personalizadas en nuestros seis pilares:</p>
+              <ul>
+                <li>Alimentacion y suplementacion</li>
+                <li>Gestion del estres y relajacion</li>
+                <li>Ritmo circadiano y sueño</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Pilares de intervencion</h3>
+              <p>Trabajaremos en revitalizar, detoxificar, optimizar y recalibrar tu organismo de forma sistematica yendo a las causas detras de tus sintomas mediante intervenciones personalizadas en nuestros seis pilares:</p>
+              
+            </div>
+
+          </div>
+          
+        </>
+        
+    )
+    }
+    
+export default QueHacemos;
