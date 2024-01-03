aeb8f0e7cbb29f8fdb2bd4bd795c5ebaa88bafd8
MaximoVeltri <maximoveltri@gmail.com>
2024-01-03 15:46:42 -0300

    QueHacemos3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import QueHacemos from "./views/QueHacemos";
-import Analisis from "./views/Analisis";
+import QueHacemos from "./views/QueHacemos/QueHacemos";
+import Analisis from "./views/AnalisisFun/Analisis";
 import Servicios from "./views/Servicios";
 import QuienesSomos from "./views/QuienesSomos";
 import Redes from "./views/Redes";
@@ -15,11 +15,10 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
-    <Footer></Footer>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/que-hacemos" element={<QueHacemos />} />
-        <Route path="/analisis-funcionales" element={<Analisis />} />
+        <Route path="/analisis" element={<Analisis />} />
         <Route path="/nuestros-servicios" element={<Servicios />} />
         <Route path="/quienes-somos" element={<QuienesSomos />} />
         <Route path="/redes" element={<Redes />} />
@@ -27,6 +26,7 @@ function App() {
         <Route path="/faqs" element={<Faqs />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+    <Footer></Footer>
     </>
   );
 }
