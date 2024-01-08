4fa27f1f867f7b672803ef8619dc19aa987b5440
daniel100f <danielvillarraga2@gmail.com>
2024-01-08 11:43:54 -0500

    contacto23

--- src/views/Contacto/Contacto.jsx
+++ src/views/Contacto/Contacto.jsx
@@ -1,32 +1,49 @@
-import "../Contacto/Contacto.css"
+import styles from "./Contacto.module.css"
+
 const Contacto=()=>{
-    return (
-        <>
-         
-            <section class="style-0">
-    <div class="style-1">
-        <h1 class="style-2"><span class="style-3" data-sr-id="0"> ¿Cómo </span><span class="style-4" data-sr-id="2"> podemos </span><span class="style-5" data-sr-id="3"> ayudarte? </span></h1>
-    </div>
-    <div class="style-6">
-        <h2 class="style-7"><span class="style-8" data-sr-id="4">Contactanos</span> <span class="style-9" data-sr-id="5">para dar</span> <span class="style-10" data-sr-id="7">tu primer paso. </span> <br class="style-11" /></h2>
-    </div>
-</section>
-<section>
-    <form>
-    <p class="style-12"><span class="style-13" data-name="mind-name"><input size="40" class="style-14" aria-required="true" aria-invalid="false" placeholder="*Nombre completo" value="" type="text" name="mind-name" /></span></p>
-    <p class="style-15"><span class="style-16" data-name="mind-tel"><input size="40" class="style-17" aria-required="true" aria-invalid="false" placeholder="*Teléfono" value="" type="tel" name="mind-tel" /></span></p>
-    <p class="style-18"><span class="style-19" data-name="mind-email"><input size="40" class="style-20" aria-required="true" aria-invalid="false" placeholder="*Mail" value="" type="email" name="mind-email" /></span></p>
-   <button class="style-21" type="submit">Enviar<span class="style-22"></span></button>
+   
+   
+ return (
+      <div className={styles.contenedor}>
 
+        <div className={styles.numbers}>
 
-            </form>
-</section>
-<section class="style-23"></section>
-            
-
-          
+        <div className={styles.tituloOne}>
+            <p className={styles.texto}>¿Cómo <br /> podemos <br /> ayudarte?</p>
+        </div>
+        <div className={styles.textoTwo}>
+            <span >Contactanos para dar tu primer paso.</span>
+        </div>
         
-        </>
+        </div>
+        
+    <div className={styles.seccion2}>
+        <div className={styles.correo}>
+            <p>hola@mindlatam.com <br />
++ 598 2604 6305 | + 598 93 982 322</p>
+
+    <p> Rostand 1577 | Of. 01 <br />
+Montevideo, Uruguay CP 11500</p>
+
+        </div>
+        <div className={styles.form}>
+            <form action="">
+            <input type="text"  placeholder="* Nombre completo"/>
+            <input type="text"  placeholder="* Teléfono"/>
+            <input type="text"  placeholder="* Mail"/>
+            <input type="text" placeholder="* Mensaje"/>
+                
+                <p className={styles.campos}> * campos obligatorios</p>
+
+                <button className={styles.enviar}>Enviar</button>
+            </form>
+
+        </div>
+
+    </div>
+    <img src="https://mindlatam.com/wp-content/themes/mind/img/contacto/contacto-1.jpg" width="100%" height="580px" alt="" />
+
+      </div>
         
     )
     }
